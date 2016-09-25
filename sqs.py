7c82a1be3f3af08a670116ff8beeda77feebf32b
Ian <wzhan366@uwo.ca>
2016-09-25 19:36:22 -0400

    Update sqs.py

--- sqs.py
+++ sqs.py
@@ -4,8 +4,8 @@ from boto.sqs.message import Message
 def sqs_write(message):
     try:
         conn = boto.sqs.connect_to_region("us-west-2",
-                        aws_access_key_id='AKIAJAJMXAWDEEIJSIDA',
-                        aws_secret_access_key='uyUIP/V6bS61ydbJGDUbjs0kgihazouaEJwsDo3Y')
+                        aws_access_key_id='your api_key_id',
+                        aws_secret_access_key='your secret_access_key')
         #get queue by queue name
         my_queue = conn.get_queue('appEngine-rpi')
         m = Message()
